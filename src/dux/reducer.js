48e51c31bac20f666115e59eb536c846716778d7
SeanBell18 <sean.bell182@gmail.com>
2018-11-29 14:51:41 -0700

    minor changes

--- src/dux/reducer.js
+++ src/dux/reducer.js
@@ -9,11 +9,33 @@ const initialState = {
     blankMemes: []
 }
 
+const ROUNDSTOWIN = 'ROUNDSTOWIN'
+const PLAYERS = 'PLAYERS'
+
 export default function reducer(state = initialState, action){
     switch(action.type){
         
+        case ROUNDSTOWIN:
+        return Object.assign({}, state, {roundsToWin: action.payload})
+
+        case PLAYERS:
+        return Object.assign({}, state, {players:action.payload})
         
         default:
         return state;
+    }
+}
+
+export function roundsToWin(val) {
+    return{
+        type: ROUNDSTOWIN,
+        payload: val
+    }
+}
+
+export function players(val) {
+    return {
+        type: PLAYERS,
+        payload: val
     }
 }
